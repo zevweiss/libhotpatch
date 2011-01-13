ea01746083ce11ca3681f00fd7cb8bfc17bec2c2
Zev Weiss <zevweiss@gmail.com>
2011-01-12 21:31:54 -0600

    Removing some vestigial cruft.
    Signal-handlers, rusage-tracking, etc from CS752 origins are no longer of
    use or interest.

--- libhotpatch.c
+++ libhotpatch.c
@@ -11,11 +11,11 @@
 #include <unistd.h>
 #include <assert.h>
 #include <dlfcn.h>
-#include <signal.h>
 #include <limits.h>
 #include <string.h>
 #include <stdint.h>
 #include <errno.h>
+
 #include <libelf.h>
 #include <gelf.h>
 
@@ -24,62 +24,9 @@
 static void libhotpatch_init(void) __attribute__((constructor));
 static void libhotpatch_fini(void) __attribute__((destructor));
 
-static void usr_handler(int signo);
+static FILE* pllog = NULL;
 
 #define BREAK() __asm__ __volatile__("int3")
-
-#define CALLWRAP(name,rettype,parmlist,callargs,extra)	  \
-	rettype name parmlist \
-	{ \
-		static typeof(&name) real_##name = NULL; \
-		if (!real_##name) { \
-			real_##name = dlsym(RTLD_NEXT,#name); \
-		} \
-		extra; \
-		return real_##name callargs; \
-	}
-
-#define CALLWRAP0(name,rettype,extra) \
-	CALLWRAP(name,rettype,(void),(),extra)
-#define CALLWRAP1(name,rettype,t1,p1,extra) \
-	CALLWRAP(name,rettype,(t1 p1),(p1),extra)
-#define CALLWRAP2(name,rettype,t1,p1,t2,p2,extra) \
-	CALLWRAP(name,rettype,(t1 p1, t2 p2),(p1,p2),extra)
-#define CALLWRAP3(name,rettype,t1,p1,t2,p2,t3,p3,extra) \
-	CALLWRAP(name,rettype,(t1 p1, t2 p2, t3 p3),(p1,p2,p3),extra)
-#define CALLWRAP4(name,rettype,t1,p1,t2,p2,t3,p3,t4,p4,extra) \
-	CALLWRAP(name,rettype,(t1 p1, t2 p2, t3 p3, t4 p4),(p1,p2,p3,p4),extra)
-#define CALLWRAP5(name,rettype,t1,p1,t2,p2,t3,p3,t4,p4,t5,p5,extra) \
-	CALLWRAP(name,rettype,(t1 p1, t2 p2, t3 p3, t4 p4, t5 p5), \
-	         (p1,p2,p3,p4,p5),extra)
-#define CALLWRAP6(name,rettype,t1,p1,t2,p2,t3,p3,t4,p4,t5,p5,t6,p6,extra) \
-	CALLWRAP(name,rettype,(t1 p1, t2 p2, t3 p3, t4 p4, t5 p5, t6 p6), \
-	         (p1,p2,p3,p4,p5,p6),extra)
-
-#define NO_EXTRA
-
-CALLWRAP0(fork,pid_t,NO_EXTRA);
-
-CALLWRAP3(read,ssize_t,int,fd,void*,buf,size_t,count,NO_EXTRA);
-CALLWRAP3(write,ssize_t,int,fd,const void*,buf,size_t,count,NO_EXTRA);
-
-CALLWRAP6(mmap,void*,void*,addr,size_t,length,
-          int,prot,int,flags,int,fd,off_t,offset,NO_EXTRA);
-CALLWRAP6(mmap2,void*,void*,addr,size_t,length,
-          int,prot,int,flags,int,fd,off_t,offset,NO_EXTRA);
-
-static FILE* pllog;
-
-#define ZERO_RUSAGE { \
-	.ru_utime = { .tv_sec = 0, .tv_usec = 0, }, \
-	.ru_stime = { .tv_sec = 0, .tv_usec = 0, }, \
-	.ru_majflt = 0, \
-	.ru_nswap = 0, \
-	.ru_nvcsw = 0, \
-	.ru_nivcsw = 0, \
-}
-
-static struct rusage tot_self_rusage = ZERO_RUSAGE, tot_children_rusage = ZERO_RUSAGE;
 
 static void* memdup(void* orig, size_t len)
 {
@@ -1562,30 +1509,15 @@ static void printargv(void)
 	fclose(argfile);
 }
 
-static void rusage_hdr(void)
-{
-	fprintf(pllog,"%6s %12s %12s %7s %9s %6s %8s %8s\n","",
-	        "User CPU","System CPU","majflt", "minflt","Swaps",
-	        "VCSW","ICSW");
-}
-
 static void print_loghdr(void)
 {
 	printargv();
-	fputc('\n',pllog);
-	rusage_hdr();
 }
 
 static void libhotpatch_init(void)
 {
-	sighandler_t ret;
 	const char* hotpatch;
 	const char* logpath;
-
-	ret = signal(SIGUSR1,usr_handler);
-	assert(ret != SIG_ERR);
-	ret = signal(SIGUSR2,usr_handler);
-	assert(ret != SIG_ERR);
 
 	logpath = getenv("LIBHOTPATCH_LOGPATH");
 	if (!logpath || !strlen(logpath))
@@ -1602,109 +1534,7 @@ static void libhotpatch_init(void)
 	}
 }
 
-static void printrusage(struct rusage* r, const char* pfx)
-{
-	fprintf(pllog,"%-6s %5li.%06li %5li.%06li",pfx,
-	        r->ru_utime.tv_sec,r->ru_utime.tv_usec,
-	        r->ru_stime.tv_sec,r->ru_stime.tv_usec);
-
-	fprintf(pllog," %7li %9li %6li %8li %8li\n",
-	        r->ru_majflt,r->ru_minflt,r->ru_nswap,r->ru_nvcsw,r->ru_nivcsw);
-
-	fflush(pllog);
-}
-
-static void rusage_sub(const struct rusage* a, const struct rusage* b,
-                       struct rusage* res)
-{
-	timersub(&a->ru_utime,&b->ru_utime,&res->ru_utime);
-	timersub(&a->ru_stime,&b->ru_stime,&res->ru_stime);
-#define SUBRUMEMB(m) res->ru_##m = a->ru_##m - b->ru_##m
-	SUBRUMEMB(maxrss);
-	SUBRUMEMB(ixrss);
-	SUBRUMEMB(idrss);
-	SUBRUMEMB(isrss);
-	SUBRUMEMB(minflt);
-	SUBRUMEMB(majflt);
-	SUBRUMEMB(nswap);
-	SUBRUMEMB(inblock);
-	SUBRUMEMB(oublock);
-	SUBRUMEMB(msgsnd);
-	SUBRUMEMB(msgrcv);
-	SUBRUMEMB(nsignals);
-	SUBRUMEMB(nvcsw);
-	SUBRUMEMB(nivcsw);
-#undef SUBRUMEMB
-}
-
-static void rusage_add(const struct rusage* a, const struct rusage* b,
-                       struct rusage* res)
-{
-	timeradd(&a->ru_utime,&b->ru_utime,&res->ru_utime);
-	timeradd(&a->ru_stime,&b->ru_stime,&res->ru_stime);
-#define ADDRUMEMB(m) res->ru_##m = a->ru_##m + b->ru_##m
-	ADDRUMEMB(maxrss);
-	ADDRUMEMB(ixrss);
-	ADDRUMEMB(idrss);
-	ADDRUMEMB(isrss);
-	ADDRUMEMB(minflt);
-	ADDRUMEMB(majflt);
-	ADDRUMEMB(nswap);
-	ADDRUMEMB(inblock);
-	ADDRUMEMB(oublock);
-	ADDRUMEMB(msgsnd);
-	ADDRUMEMB(msgrcv);
-	ADDRUMEMB(nsignals);
-	ADDRUMEMB(nvcsw);
-	ADDRUMEMB(nivcsw);
-#undef ADDRUMEMB
-}
-
-static void showstats(int abs)
-{
-	struct rusage newself, newchildren, deltaself, deltachildren;
-	struct rusage* sprint;
-	struct rusage* cprint;
-	char pfxbuf[64]; // HACK
-
-	getrusage(RUSAGE_SELF,&newself);
-	getrusage(RUSAGE_CHILDREN,&newchildren);
-
-	rusage_sub(&newself,&tot_self_rusage,&deltaself);
-	rusage_sub(&newchildren,&tot_children_rusage,&deltachildren);
-
-	memcpy(&tot_self_rusage,&newself,sizeof(struct rusage));
-	memcpy(&tot_children_rusage,&newchildren,sizeof(struct rusage));
-
-	sprint = abs ? &newself : &deltaself;
-	cprint = abs ? &newchildren : &deltachildren;
-
-	snprintf(pfxbuf,sizeof(pfxbuf),"%i",getpid());
-	printrusage(sprint,pfxbuf);
-
-	if (cprint->ru_utime.tv_sec || cprint->ru_utime.tv_usec
-	    || cprint->ru_stime.tv_sec || cprint->ru_stime.tv_usec
-            || cprint->ru_majflt || cprint->ru_nswap
-	    || cprint->ru_nvcsw || cprint->ru_nivcsw) {
-		snprintf(pfxbuf,sizeof(pfxbuf),"%i>",getpid());
-		printrusage(cprint,pfxbuf);
-	}
-}
-
-static void usr_handler(int signo)
-{
-	showstats(signo == SIGUSR1);
-}
-
 static void libhotpatch_fini(void)
 {
-	const char* envtot;
-	signal(SIGUSR1,SIG_DFL);
-
-	envtot = getenv("LIBHOTPATCH_PRINT_TOTALS");
-	if (envtot && strlen(envtot)) {
-		fprintf(pllog,"Totals:\n");
-		showstats(1);
-	}
 	fclose(pllog);
 }
